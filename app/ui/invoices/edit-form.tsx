97147c6e1fbc6019d1bc0ce9750e1d560f8d55d7
Hollypie <hbriggs@byupathway.edu>
2025-11-19 20:51:09 -0700

    fixed a syntax error. I had a comment where I cannot have one

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -25,9 +25,9 @@ export default function EditInvoiceForm({
 
   return (
     <form action={updateInvoiceWithId}>
-      {/* ... */}
+      
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
-        {/* Customer Name */}
+        
         <div className="mb-4">
           <label htmlFor="customer" className="mb-2 block text-sm font-medium">
             Choose customer
